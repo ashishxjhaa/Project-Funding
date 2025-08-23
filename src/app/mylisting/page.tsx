74d55978e62c5081f82265810ead9b61f4806d60
Ashish Jha <ashishxyzjha@gmail.com>
2025-08-23 22:43:24 +0530

    Alignment Fix

--- src/app/mylisting/page.tsx
+++ src/app/mylisting/page.tsx
@@ -69,6 +69,9 @@ export default function MyListingPage() {
       <div className="pt-30 pr-10 flex justify-between">
         <Back />
         <UploadProject />
+      </div>
+
+      <div>
 
         <UploadProject open={!!editingProject}
           initialProject={editingProject}
