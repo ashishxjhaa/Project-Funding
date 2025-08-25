6cde8bf5d9446bf95a672bfd6123027db843eccd
Ashish Jha <ashishxyzjha@gmail.com>
2025-08-25 20:00:35 +0530

    Progess bar Done

---
+++ src/components/TopLoader.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import LoadingBar, { LoadingBarRef } from "react-top-loading-bar";
+import { usePathname } from "next/navigation";
+
+export default function TopLoader() {
+    const ref = useRef<LoadingBarRef>(null);
+    const pathname = usePathname();
+
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.continuousStart();
+            ref.current.complete();
+        }
+    }, [pathname]);
+
+  return (
+    <LoadingBar
+      color="#3b82f6"
+      height={6}
+      shadow={true}
+      ref={ref}
+      style={{ zIndex: 9999 }}
+    />
+  );
+}
