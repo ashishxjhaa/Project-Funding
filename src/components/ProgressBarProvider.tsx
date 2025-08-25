ea3450a3ba05053167269d18ceaf22623fd6cfe1
Ashish Jha <ashishxyzjha@gmail.com>
2025-08-25 17:47:55 +0530

    Progress bar

---
+++ src/components/ProgressBarProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <ProgressBar
+        height="4px"
+        color="#fffd00"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+      {children}
+    </>
+  );
+};
+
+export default Providers;
