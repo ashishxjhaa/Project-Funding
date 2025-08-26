652225c7e8a8e3279f9fe35689b144e44f353634
Ashish Jha <ashishxyzjha@gmail.com>
2025-08-26 17:28:07 +0530

    Alignment

--- src/app/listing/page.tsx
+++ src/app/listing/page.tsx
@@ -15,6 +15,9 @@ function Listing() {
       <ListingNavbar search={search} setSearch={setSearch} filterTags={filterTags} setFilterTags={setFilterTags} />
       <ListingWelcome />
       <ListingContent search={search} filterTags={filterTags} />
+      <button className="fixed bottom-8 right-7 z-50 rounded-full p-4 shadow-xl bg-[#FF8162] text-white cursor-pointer" aria-label="Open chats"> 
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-messages-square-icon lucide-messages-square"><path d="M16 10a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 14.286V4a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/><path d="M20 9a2 2 0 0 1 2 2v10.286a.71.71 0 0 1-1.212.502l-2.202-2.202A2 2 0 0 0 17.172 19H10a2 2 0 0 1-2-2v-1"/></svg> 
+      </button>
     </div>
   )
 }
