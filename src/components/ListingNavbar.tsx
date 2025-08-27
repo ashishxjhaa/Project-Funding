2c39f2bdcb592c552b2a32d2f28f9cc99214576c
Ashish Jha <ashishxyzjha@gmail.com>
2025-08-27 13:00:14 +0530

    Navbar Responsive

--- src/components/ListingNavbar.tsx
+++ src/components/ListingNavbar.tsx
@@ -112,12 +112,12 @@ function ListingNavbar({ search, setSearch, filterTags, setFilterTags }: { searc
                     </div>
                 </div>
 
-                <div className="flex items-center gap-6 flex-1 justify-center min-w-0">
-                    <div className="flex items-center gap-3 border-white border-[1.5px] rounded-lg p-2 min-w-0 max-w-[140px] sm:max-w-xs md:max-w-xs">
+                <div className="flex items-center gap-6 flex-1 justify-center">
+                    <div className="flex items-center gap-3 border-white border-[1.5px] rounded-lg p-2 min-w-0 max-w-full sm:w-auto md:max-w-xs">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search-icon lucide-search text-white ml-2"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
                         <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Search" className="placeholder-white focus:outline-none w-full sm:w-auto" />
                     </div>
-                    <div onClick={() => setShowFilterMenu(!showFilterMenu)} className="flex items-center gap-4 hover:bg-white/30 rounded-xl px-7 py-2 w-fit cursor-pointer">
+                    <div onClick={() => setShowFilterMenu(!showFilterMenu)} className="flex items-center gap-4 hover:bg-white/30 rounded-xl px-5 py-2 w-full sm:w-fit justify-center cursor-pointer shrink-3">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sliders-horizontal-icon lucide-sliders-horizontal"><line x1="21" x2="14" y1="4" y2="4"/><line x1="10" x2="3" y1="4" y2="4"/><line x1="21" x2="12" y1="12" y2="12"/><line x1="8" x2="3" y1="12" y2="12"/><line x1="21" x2="16" y1="20" y2="20"/><line x1="12" x2="3" y1="20" y2="20"/><line x1="14" x2="14" y1="2" y2="6"/><line x1="8" x2="8" y1="10" y2="14"/><line x1="16" x2="16" y1="18" y2="22"/></svg>
                         <div className="font-medium tracking-wider text-white text-lg">Filter</div>
                     </div>
@@ -141,11 +141,11 @@ function ListingNavbar({ search, setSearch, filterTags, setFilterTags }: { searc
                     </div>
                 )}
 
-                <div ref={menuRef} className="relative flex items-center justify-end flex-none shrink-0">
-                    <div onClick={() => setOpenProfile(true)} className="group flex gap-4 items-center hover:bg-[#FEB57F] cursor-pointer rounded-4xl px-2 py-1.5">
-                        <div className="w-10 h-10 bg-gray-600 flex items-center justify-center font-bold text-xl rounded-full hover:text-white">{user?.fullName?.[0]}</div>
-                        <div className="text-white font-medium group-hover:text-black">{user?.fullName}
-                            <div className="text-slate-300 font-normal text-sm group-hover:text-black tracking-wide">{user?.email}</div>
+                <div ref={menuRef} className="relative flex items-center justify-end flex-none">
+                    <div onClick={() => setOpenProfile(true)} className="group flex items-center gap-2 sm:gap-4 max-w-full sm:max-w-none overflow-hidden hover:bg-[#FEB57F] cursor-pointer rounded-4xl px-2 py-1.5">
+                        <div className="w-10 h-10 bg-gray-600 flex items-center justify-center font-bold text-xl rounded-full shrink-0 hover:text-white">{user?.fullName?.[0]}</div>
+                        <div className="hidden sm:block text-white font-medium group-hover:text-black">{user?.fullName}
+                            <div className="text-slate-300 font-normal text-sm group-hover:text-black tracking-wide truncate">{user?.email}</div>
                         </div>
                         <div>
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down-icon lucide-chevron-down group-hover:text-black"><path d="m6 9 6 6 6-6"/></svg>
@@ -153,7 +153,7 @@ function ListingNavbar({ search, setSearch, filterTags, setFilterTags }: { searc
                     </div>
 
                     {openProfile && (
-                        <div className="absolute top-full w-80 p-4">
+                        <div className="absolute top-full w-80 p-4 max-h-[80vh] max-w-[90vw] overflow-auto">
                             {/* First Part */}
                             <div className="bg-[#D69B6F] flex flex-col gap-3 p-4 rounded-t-lg">
                                 <div className="flex items-center gap-3">
