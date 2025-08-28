83551edc8f6cfe5c5d588cbdeeefa6f6af289799
Ashish Jha <ashishxyzjha@gmail.com>
2025-08-28 08:20:19 +0530

    Responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -120,7 +120,7 @@ function Hero() {
                         </div>
                     </div>
 
-                    <div className="glass-card h-[190px] w-full max-w-[420px] flex flex-col p-4 mx-auto bg-gray-950 border border-gray-100/10 rounded-lg cursor-pointer hover:scale-110 hover:-translate-y-2 transition-transform duration-300">
+                    <div className="glass-card h-[200px] w-full max-w-[420px] flex flex-col p-4 mx-auto bg-gray-950 border border-gray-100/10 rounded-lg cursor-pointer hover:scale-110 hover:-translate-y-2 transition-transform duration-300">
                         <div className="flex items-center gap-2 mb-2">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-icon lucide-user my-2 mb-1 text-orange-400"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
                             <span className="text-xs font-semibold text-slate-100">Ravi Shankar</span>
@@ -171,7 +171,7 @@ function Hero() {
                         </div>
                     </div>
 
-                    <div className="glass-card h-[170px] w-full max-w-[420px] flex flex-col p-4 mx-auto bg-gray-950 border border-gray-100/10 rounded-lg cursor-pointer hover:scale-110 hover:-translate-y-2 transition-transform duration-300">
+                    <div className="glass-card h-[180px] w-full max-w-[420px] flex flex-col p-4 mx-auto bg-gray-950 border border-gray-100/10 rounded-lg cursor-pointer hover:scale-110 hover:-translate-y-2 transition-transform duration-300">
                         <div className="flex items-center gap-2 mb-2">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-icon lucide-user my-2 mb-1 text-orange-400"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
                             <span className="text-xs font-semibold text-slate-100">Vikas Patil</span>
